d0dfde97ae3cd6a8a042a567d9d66061de938685
Sid Price <sid@sidprice.com>
2025-03-21 16:05:24 -0600

    ctxlink/ublox_server: Primitive, not yet working, implementation of a ublox server

--- src/platforms/ctxlink/ublox_server.c
+++ src/platforms/ctxlink/ublox_server.c
@@ -40,6 +40,36 @@
 
 static _Atomic bool wifi_awake = false;
 
+/*
+	The uBlox module has initialized the SPI interface when
+	MISO is low, so include that in the test for data ready
+ */
+void ublox_wait_for_drdy_and_miso(void)
+{
+	while (true) {
+		int drdy = gpio_get(WINC1500_PORT, WINC1500_IRQ);
+		int miso = gpio_get(WINC1500_SPI_DATA_PORT, WINC1500_SPI_MISO);
+		if (miso == 0) {
+			if (drdy == 1)
+				break;
+		}
+		platform_delay(1);
+	}
+}
+
+void ublox_wait_for_drdy(void)
+{
+	while (gpio_get(WINC1500_PORT, WINC1500_IRQ) == 0) {
+		// int drdy = GPIO_IDR(WINC1500_PORT) & WINC1500_IRQ;
+		// int miso = GPIO_IDR(WINC1500_SPI_DATA_PORT) & WINC1500_SPI_MISO;
+		// if (miso == 0) {
+		// 	if (drdy == 1)
+		// 		break;
+		// }
+		platform_delay(1);
+	}
+}
+
 void exti9_5_isr(void)
 {
 	//
@@ -48,7 +78,111 @@ void exti9_5_isr(void)
 	if (exti_get_flag_status(EXTI9) == EXTI9) {
 		// Reset the interrupt state
 		exti_reset_request(EXTI9);
+		//
+		// The uBlox module is ready to initialize SPI
+		// disable this interrupt
+		//
+		exti_disable_request(WINC1500_IRQ);
+		nvic_disable_irq(NVIC_EXTI9_5_IRQ);
 		__atomic_store_n(&wifi_awake, true, __ATOMIC_RELAXED);
+	}
+}
+
+/*
+	The format of a module -> host packet is:
+		Header byte 1: 0xBA
+		Header byte 2: 0x15
+		Length byte 1: MSB of the length	<-- Bytes following this 16-bit length
+		Length byte 2: LSB of the length
+		Payload: The actual data
+*/
+bool ublox_read_packet(uint8_t *buffer, uint32_t length, bool first_contact)
+{
+	(void)length;
+	uint8_t *buffer_start = buffer;
+	uint16_t payload_size;
+	if (first_contact == true) {
+		/*
+			Wait for the DRDY signal to go high
+		*/
+		ublox_wait_for_drdy();
+	} else {
+		/*
+			Wait for DRDY to go high and miso low 
+			
+			miso being low indicates the uBlox module has initialized the SPI interface
+		*/
+		ublox_wait_for_drdy_and_miso();
+	}
+	/*
+		Wait for the DRDY signal to go high
+	*/
+	ublox_wait_for_drdy();
+	/*
+		Asserting the chip select signals a packet read start
+	*/
+	gpio_clear(WINC1500_PORT, WINC1500_SPI_NCS);
+
+	for (uint32_t index = 0; index < 4; index++) { // Read the header and byte count
+		*buffer = spi_xfer(WINC1500_SPI_CHANNEL, *buffer);
+		buffer++;
+	}
+	/*
+		Check for valid header
+	*/
+	if (buffer_start[0] != 0xBA && buffer_start[1] != 0x15) {
+		/*
+			Invalid header, reset the chip select and return
+		*/
+		gpio_set(WINC1500_PORT, WINC1500_SPI_NCS);
+		return false;
+	}
+	/*
+		Now read the payload
+	*/
+	payload_size = (buffer_start[2] << 8) | buffer_start[3];
+	if (payload_size == 0)
+		return false;
+
+	for (uint32_t index = 0; index < payload_size; index++) {
+		*buffer = spi_xfer(WINC1500_SPI_CHANNEL, *buffer);
+		buffer++;
+	}
+	gpio_set(WINC1500_PORT, WINC1500_SPI_NCS);
+	return true;
+}
+
+void ublox_write_packet(uint8_t *buffer, uint32_t length)
+{
+	/*
+		Asserting the chip select signals a packet start
+	*/
+	gpio_clear(WINC1500_PORT, WINC1500_SPI_NCS);
+
+	for (uint32_t index = 0; index < length; index++)
+		spi_xfer(WINC1500_SPI_CHANNEL, *buffer++);
+	gpio_set(WINC1500_PORT, WINC1500_SPI_NCS);
+}
+
+/*
+	To request the uBlox module uses the SPI interface, it
+	requires SPI_CLK activity. This function activates SPI_CS
+	and writes 8 bytes to the uBlox module.
+ */
+void ublox_spi_wakeup(void)
+{
+	gpio_clear(WINC1500_PORT, WINC1500_SPI_NCS);
+
+	for (uint32_t index = 0; index < 8; index++) {
+		spi_xfer(WINC1500_SPI_CHANNEL, 0);
+	}
+	gpio_set(WINC1500_PORT, WINC1500_SPI_NCS);
+}
+
+static inline void short_delay(void)
+{
+	for (volatile int i = 0; i < 1000; i++) {
+		__asm__("nop");
 	}
 }
 
@@ -58,7 +192,38 @@ void app_initialize(void)
 	//
 	// Hold here wi-fi module to wake up
 	//
-	while (__atomic_load_n(&wifi_awake, __ATOMIC_RELAXED) == false)
+	while (__atomic_load_n(&wifi_awake, __ATOMIC_RELAXED) == false) {
+		platform_delay(1);
+	}
+
+	// The uBlox module requires SPI activity to enable the SPI interface
+	// to be used for AT commands
+	//
+	// ublox_spi_wakeup();
+	uint8_t input[32] = {0};
+
+	while (true) {
+		if (ublox_read_packet(input, 32, true)) {
+			if (input[0] == 0xba && input[1] == 0x15) {
+				// This is a valid packet
+				break;
+			}
+		}
+		memset(input, 0, 32);
+		short_delay();
+	}
+	//
+	// Let's make a  test by sending AT and looking for a reply
+	//
+	uint8_t output[32] = {0xba, 0x15, 0x00, 0x0c, 'A', 'T', 'S', '5', '?', '\r', '\n', 0xff, 0xff, 0xff, 0xff, 0xff};
+	ublox_write_packet(output, 16);
+	platform_delay(1);
+	memset(input, 0xff, 32);
+	while (ublox_read_packet(input, 32, true) == false)
+		platform_delay(1);
+	memset(input, 0xff, 32);
+	platform_delay(1);
+	while (ublox_read_packet(input, 32, true) == false)
 		platform_delay(1);
 	platform_delay(100);
 }
